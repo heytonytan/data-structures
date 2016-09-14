23333de81ae3e69136fc9d82d5e33b1b8faa1203
Tony K Tan <tankwantiong@gmail.com>
2016-09-14 12:24:20 -0700

    Pass test to report size of 2 for queue (functional pattern)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,16 +4,20 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  // Use counter for assigning numeric keys
+  var counter = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    counter++;
   };
 
   someInstance.dequeue = function() {
   };
 
   someInstance.size = function() {
-    return 0;
+    return counter;
   };
 
   return someInstance;
